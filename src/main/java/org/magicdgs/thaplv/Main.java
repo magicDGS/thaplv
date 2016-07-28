fe892e10e18d392892dd25753635ddca9467a9ae
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2016-07-28 12:48:56 +0200

    build, main class and basic test structure

---
+++ src/main/java/org/magicdgs/thaplv/Main.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016, Daniel Gomez-Sanchez <daniel.gomez.sanchez@hotmail All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.magicdgs.thaplv;
+
+import org.broadinstitute.hellbender.cmdline.CommandLineProgram;
+import org.broadinstitute.hellbender.exceptions.UserException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main class for thaplv
+ *
+ * @author Daniel Gomez-Sanchez
+ */
+public class Main {
+
+    /**
+     * The packages we wish to include in our command line.
+     */
+    protected static List<String> getPackageList() {
+        final List<String> packageList = new ArrayList<>();
+        // TODO: remove hellbender tools
+        packageList.add("org.broadinstitute.hellbender");
+        packageList.add("org.magicdgs.patok.tools");
+        return packageList;
+    }
+
+    /**
+     * Entry point for thaplv.
+     */
+    public static void main(final String[] args) {
+        System.out.println(
+                "This is a clone for GATK4 command line and should not be taken as a thaplv distribution yet");
+        try {
+            Object result = new Main().instanceMain(args);
+            if (result != null) {
+                System.out.println("Tool returned:\n" + result);
+            }
+        } catch (final UserException.CommandLineException e) {
+            // the GATK framework already prints the error
+            System.exit(1);
+        } catch (final UserException e) {
+            // this prints the error for user exceptions
+            CommandLineProgram.printDecoratedUserExceptionMessage(System.err, e);
+            System.exit(2);
+        } catch (final Exception e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+    }
+
+    /**
+     * This is a necessary hack for use {@linl CommandLineProgramTest}
+     */
+    public Object instanceMain(final String[] args) {
+        return new org.broadinstitute.hellbender.Main()
+                .instanceMain(args, getPackageList(), "thaplv");
+    }
+}
---
+++ src/main/java/org/magicdgs/thaplv/utils/test/BaseTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2016, Daniel Gomez-Sanchez <daniel.gomez.sanchez@hotmail All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.magicdgs.thaplv.utils.test;
+
+import htsjdk.samtools.util.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.broadinstitute.hellbender.utils.LoggingUtils;
+import org.broadinstitute.hellbender.utils.io.IOUtils;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
+
+/**
+ * Base test class for thaplv tests. All test cases should extend from this class;
+ * it sets up the logger, and resolves the location of directories that we rely on.
+ *
+ * This class have a lot of code from {@link org.broadinstitute.hellbender.utils.test.BaseTest} and
+ * also contains several static methods useful for testing.
+ *
+ * Test should be named as following:
+ *
+ * 1) ClassNameUnitTest for unit tests of single classes
+ * 2) ClassNameIntegrationTest for tool tests
+ * 3) ClassNameTest for other tests
+ *
+ * In addition, any resource file specific for a class should be sited under
+ * {@link #TEST_ROOT_FILE_DIRECTORY}/package_path/ClassName/testFile
+ *
+ * @author Daniel Gomez-Sanchez (magicDGS)
+ */
+public abstract class BaseTest {
+
+    @BeforeSuite
+    public void setTestVerbosity() {
+        LoggingUtils.setLoggingLevel(Log.LogLevel.DEBUG);
+    }
+
+    public static final Logger logger = LogManager.getLogger("org.magicdgs.thaplv");
+
+    private static final String CURRENT_DIRECTORY = System.getProperty("user.dir");
+
+    // directory for all test files
+    private static final String TEST_ROOT_FILE_DIRECTORY =
+            new File(CURRENT_DIRECTORY, "src/test/resources/").getAbsolutePath();
+
+    /** Log this message so that it shows up inline during output as well as in html reports */
+    public static void log(final String message) {
+        Reporter.log(message, true);
+    }
+
+    /** Default tolerance for float and double tests */
+    public static double DEFAULT_TOLERANCE = 1e-6; // quite stringent
+
+    /** Returns the location of the resource directory for the tested class */
+    public String getTestDataDir() {
+        return TEST_ROOT_FILE_DIRECTORY + this.getClass().getPackage().getName().replace(".", "/")
+                + "/" + getTestedClassName() + "/";
+    }
+
+    /**
+     * Get a test file resolved using {@link #getTestDataDir()} as the parent
+     *
+     * @param fileName the name of a file
+     *
+     * @return the test file
+     */
+    public File getTestFile(String fileName) {
+        return new File(getTestDataDir(), fileName);
+    }
+
+    /**
+     * Creates a temp file that will be deleted on exit after tests are complete.
+     *
+     * This will also mark the corresponding Tribble/Tabix/BAM indices matching the temp file for
+     * deletion.
+     *
+     * @param name      Prefix of the file.
+     * @param extension Extension to concat to the end of the file.
+     *
+     * @return A file in the temporary directory starting with name, ending with extension, which
+     * will be deleted after the program exits.
+     */
+    public static File createTempFile(final String name, final String extension) {
+        return IOUtils.createTempFile(name, extension);
+    }
+
+    /**
+     * Creates an empty temp directory which will be deleted on exit after tests are complete
+     *
+     * @param prefix prefix for the directory name
+     *
+     * @return an empty directory starting with prefix which will be deleted after the program exits
+     */
+    public static File createTempDir(final String prefix) {
+        final File dir = IOUtils.tempDir(prefix, "");
+        IOUtils.deleteRecursivelyOnExit(dir);
+        return dir;
+    }
+
+    /** Returns the name of the class being tested */
+    public final String getTestedClassName() {
+        if (getClass().getSimpleName().contains("IntegrationTest")) {
+            return getClass().getSimpleName().replaceAll("IntegrationTest$", "");
+        } else if (getClass().getSimpleName().contains("UnitTest")) {
+            return getClass().getSimpleName().replaceAll("UnitTest$", "");
+        } else {
+            return getClass().getSimpleName().replaceAll("Test$", "");
+        }
+    }
+}
